e9a27f4486ce474b4828bf00a848f109d10c39b3
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-05-03 10:50:04 +1000

    Add project files.

---
+++ NegativeBinomialDistribution/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NegativeBinomialDistribution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Binomial Probability {BinomialDistribution(9, 2, 0.5)}");
+            Console.WriteLine($"Negative Binomial Probability {NegBinomialDistribution(10, 3, 0.5)}");
+        }
+
+        public static double BinomialDistribution(int numberOfTrials, int numberOfSuccesses, double probOfSuccess)
+        {
+            var nCr = Factorial(numberOfTrials) / (Factorial(numberOfSuccesses) * Factorial(numberOfTrials - numberOfSuccesses));
+
+            return nCr * Math.Pow(probOfSuccess, numberOfSuccesses) * Math.Pow((1 - probOfSuccess), (numberOfTrials - numberOfSuccesses));
+        }
+
+        /// <summary>
+        /// Gives the probability of rth success in n trials
+        /// </summary>
+        /// <param name="numberOfTrials"></param>
+        /// <param name="numberOfSuccesses"></param>
+        /// <param name="probOfSuccess"></param>
+        /// <returns></returns>
+        public static double NegBinomialDistribution(int numberOfTrials, int numberOfSuccesses, double probOfSuccess)
+        {
+            var nCr = Factorial(numberOfTrials - 1) / (Factorial(numberOfSuccesses - 1) * Factorial(numberOfTrials - numberOfSuccesses));
+
+            return nCr * Math.Pow(probOfSuccess, numberOfSuccesses) * Math.Pow((1 - probOfSuccess), (numberOfTrials - numberOfSuccesses));
+        }
+
+        public static double NegBinomialDistributionOriginal(int numberOfTrials, int numberOfSuccesses, double probOfSuccess)
+        {
+            var nCr = Factorial(numberOfTrials - 1) / (Factorial(numberOfSuccesses - 1) * Factorial(numberOfTrials - numberOfSuccesses));
+
+            return nCr * Math.Pow(probOfSuccess, numberOfSuccesses) * Math.Pow((1 - probOfSuccess), (numberOfTrials - numberOfSuccesses));
+        }
+
+
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            long answer = 1;
+            for (long i = 1; i <= n; i++)
+            {
+                answer = answer * i;
+            }
+
+            return answer;
+        }
+    }
+}
